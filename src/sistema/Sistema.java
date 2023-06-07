9c541a96c0c500820e2e23af842d5ba64055818e
Tito Jaramillo <titoj740@gmail.com>
2023-06-07 09:33:41 -0500

    Cambios en clase Sistema, métodos de busqueda

--- src/sistema/Sistema.java
+++ src/sistema/Sistema.java
@@ -6,6 +6,7 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 import clases.Persona;
+import clases.TipoCuenta;
 
 /**
  * Clase para el manejo de las funcionalidades del sistema
@@ -28,5 +29,41 @@ public class Sistema {
 
     /**
      * METODOS REFERENTES A MANEJO DE USUARIOS
+     */
+
+    public boolean agregarUsuario(Persona nuevo){
+        return usuarios.add(nuevo);
+    }
+
+    /**
+     * Recorre los usuarios en el sistema y devuelve aquellos que son de cierto tipo específico,
+     * puede ser un solo tipo o varios
+     * @param tipo los tipos de usuarios que están buscando, puede ser uno o más tipos
+     * @return SortedSet de los usuarios que están dentro de la lista de tipos pasada por parámtro
+     */
+    public SortedSet<Persona> buscarUsuarioTipo(ArrayList<TipoCuenta> tipo){
+        SortedSet<Persona> resultado = new TreeSet<>();
+
+        for (Persona p: usuarios){
+            if (tipo.contains(p.getTipo())){
+                resultado.add(p);
+            }
+        }
+
+        return resultado;
+    }
+
+    /**
+     * Busqueda binaria de unusuario por su cédula
+     * @param cedula cédula a buscar
+     * @return Un objeto de tipo Persona de encontrarse o null si no existe
+     */
+    public Persona buscarUsuarioCedula(String cedula){
+
+        return null;
+    }
+
+    /**
+     * MÉTODOS PARA MANEJAR LAS SUCURSALES Y SUS ATRIBUTOS
      */
 }
